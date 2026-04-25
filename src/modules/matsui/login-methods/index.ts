839ec133c2543c49ebfd31a5b433b8ea1688013b
na3shkw <48012204+na3shkw@users.noreply.github.com>
2026-04-25 10:52:05 +0900

    refactor: LoginMethodNameをエクスポートしないように変更

--- src/modules/matsui/login-methods/index.ts
+++ src/modules/matsui/login-methods/index.ts
@@ -2,7 +2,7 @@ import type { MatsuiLoginMethod } from "./login-method.js";
 import { PasskeyLoginMethod } from "./passkey/index.js";
 import { PasswordLoginMethod } from "./password/index.js";
 
-export type LoginMethodName = "password" | "passkey";
+type LoginMethodName = "password" | "passkey";
 
 export class LoginMethodFactory {
   static create(methodName: LoginMethodName): MatsuiLoginMethod {
